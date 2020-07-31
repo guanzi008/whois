4450e7c3e7e3981191cce1023d01664f837e3a0a
罐子 <admin@odata.cc>
2020-07-31 21:05:20 +0800

    whois

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	if len(os.Args) > 1 {
 		result, err := www.GetWhois(os.Args[1])
 		if err != nil {
-			fmt.Println("Error in whois lookup : %v ", err)
+			fmt.Printf("Error in whois lookup : %v \n", err)
 		} else {
 			fmt.Println(result)
 			fmt.Printf("Nameservers: %v \n", www.ParseNameServers(result))
